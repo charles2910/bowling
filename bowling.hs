f136fc613a6b6bd158c05822fbec204a3eefe4f1
Carlos Henrique Lima Melara <charlesmelara@riseup.net>
2023-06-05 00:08:08 -0300

    Correct End Frame and print it correctly

--- bowling.hs
+++ bowling.hs
@@ -21,8 +21,9 @@ main = do
         let plays_int = [read x :: Integer | x <- plays]
         -- putStrLn (show plays)
         -- putStrLn (show plays_int)
-        -- putStrLn (show (loadFrames plays_int))
-        putStrLn (printFrames (loadFrames plays_int) ++ show (calcPoints (loadFrames plays_int)))
+        let frames = checkEndFrame (loadFrames plays_int)
+        putStrLn (show frames)
+        putStrLn (printFrames frames ++ show (calcPoints frames))
 
 data Play = Normal | Strike | Spare
         deriving (Show, Eq)
@@ -43,17 +44,30 @@ loadFrames (h:t)
         | h + (head t) == 10 = Frame { plays = (h, head t), playType = Spare, bonus = 1, bonusPlay = False, bonusPlayScore = 0 } : loadFrames (tail t)
         | otherwise = Frame { plays = (h, head t), playType = Normal, bonus = 0, bonusPlay = False, bonusPlayScore = 0 } : loadFrames (tail t)
 
+checkEndFrame :: [Frame] -> [Frame]
+checkEndFrame frames
+        | length frames == 12 = (take 9 frames) ++ [Frame { plays = (fst (plays (frames !! 9)), fst (plays (frames !! 10))), playType = playType (frames !! 9), bonus = 1, bonusPlay = True, bonusPlayScore = fst (plays (frames !! 11)) }]
+        | otherwise = frames
+
 printFrames :: [Frame] -> [Char]
 printFrames [] = []
 printFrames (h:t)
-        | bonusPlay h == True  = printPlay (plays h) ++ " " ++ show (bonusPlayScore h) ++ " | " ++ printFrames t
+        | bonusPlay h == True  = printPlay (plays h) ++ " " ++ printBonusPlay (fst $ plays h, snd $ plays h, bonusPlayScore h) ++ " | " ++ printFrames t
         | bonusPlay h == False = printPlay (plays h)  ++ " | " ++ printFrames t
 
 printPlay :: (Integer, Integer) -> [Char]
 printPlay (p1, p2)
-        | p1 == 10 = "X _"
+        | p1 == 10 && p2 == 0 = "X _"
         | p1 + p2 == 10 = show p1 ++ " /"
+        | p1 + p2 == 20 = "X X"
+        | p1 + p2 > 10 = "X " ++ show p2
         | otherwise = show p1 ++ " " ++ show p2
+
+printBonusPlay :: (Integer, Integer, Integer) -> [Char]
+printBonusPlay (p1, p2, bonus) 
+        | p2 == 10 && bonus == 10 = "X"
+        | p1 == 10 && p2 + bonus == 10 = "/"
+        | otherwise = show bonus
 
 calcPoints :: [Frame] -> Integer
 calcPoints [] = 0
@@ -61,10 +75,18 @@ calcPoints (h:t)
         | playType h == Normal = sumPlayScore (plays h) + bonusPlayScore h + calcPoints t
         | playType h == Spare && bonusPlay h == False = sumPlayScore (plays h)  + fst (plays (head t)) +  calcPoints t
         | playType h == Spare && bonusPlay h == True = sumPlayScore (plays h)  + bonusPlayScore h +  calcPoints t
-        | playType h == Strike = 10 + fst (plays (head t)) + snd (plays (head t)) + calcPoints t
+        | playType h == Strike = 10 + sumPlayScore (getNextTwoScores (h:t)) + calcPoints t
 
 sumPlayScore :: (Integer, Integer) -> Integer
 sumPlayScore (p1, p2)
         | p1 == 10 = 10
         | p1 + p2 == 10 = 10
         | otherwise = p1 + p2
+
+getNextTwoScores :: [Frame] -> (Integer, Integer)
+getNextTwoScores [] = (0, 0)
+getNextTwoScores (h:t)
+        | playType (head t) == Normal || playType (head t) == Spare = plays (head t)
+        | playType h == Strike && bonusPlay h == True = (snd (plays h), bonusPlayScore h)
+        | playType (head t) == Strike && bonusPlay (head t) == True = plays (head t)
+        | playType (head t) == Strike = (10, fst (plays (head (tail t))))
